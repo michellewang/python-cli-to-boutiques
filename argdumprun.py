134d78defae02d71d31bd715793ce1c65d103914
Melanie Garcia <melaniegarcia790@gmail.com>
2026-06-12 11:37:51 +0200

    Saving json with json package to format it nicely.

--- argdumprun.py
+++ argdumprun.py
@@ -1,7 +1,12 @@
 import argdump
 from .parser import _build_parser
+import json
+
 
 parser = _build_parser()
 
 json_str = argdump.dumps(parser) 
-print(json_str)
+# print(json_str)
+
+with open("mriqc.json", "w") as f:
+    json.dump(json.loads(json_str), f, indent=2)
